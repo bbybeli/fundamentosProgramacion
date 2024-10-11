b449ef2dbf0879591efec13f414bf6e6ff93c9d5
Aleets <alex_26_azul@hotmail.com>
2024-10-11 13:57:17 -0600

    ejercicio3

--- ejercicio3.py
+++ ejercicio3.py
@@ -10,7 +10,10 @@ hipotenusa: float -> c
 Analisis:
   Se utiliza el teorema de pitagoras
 '''
-import Math
+
 a= input("Escribe el valor del cateto A:")
 a= int (a)
 b= input ("Escribe el valor del cateto B")
+b= int(b)
+c= (a*a+b*b)**(1/2)
+print("El valor de la hipotenusa es:", c)
