db7fdfba3acf037671433f596f7b923700f8e534
Aleets <alex_26_azul@hotmail.com>
2024-10-14 11:59:47 -0600

    ejercicio4

---
+++ ejercicio4.py
@@ -0,0 +1,23 @@
+'''
+Ejercicio 4:
+Programa  que lea   dos  numeros y  muestre, la suma, la resta,la division,la multiplicacion 
+Entradas:
+     numero1:integer
+      numero2:integer
+Salidas:
+resultado de la suma,resta,division,multiplicacion
+
+
+'''
+
+print("Ehhh")
+numero1=int(input("Ingrese el primer numero:"))
+numero2=int(input("Ingrese el segunda numero:"))
+suma=(numero1+numero2)
+resta=(numero1-numero2)
+division=(numero1/numero2)
+multiplicacion=(numero1*numero2)
+print("La suma es:",suma)
+print("La resta es:",resta)
+print("La division es:",division)
+print("La multiplicacion es:",multiplicacion)
